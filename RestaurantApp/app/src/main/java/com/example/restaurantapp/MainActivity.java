0d5db7ade650f51fed64bb69c28c5bd9c20d557d
Veselin Velkov <vesselin60@gmail.com>
2022-12-12 17:51:44 +0200

    35. CardView

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView startersCard;
+    CardView mainsCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
