2e1067f0b3adcb0387f2f250eb5233bae0cfa940
Veselin Velkov <vesselin60@gmail.com>
2022-12-13 14:09:17 +0200

    40. ListView

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/StartersActivity.java
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/StartersActivity.java
@@ -2,7 +2,9 @@ package com.example.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.lights.LightState;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class StartersActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class StartersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starters);
+
+        ListView startersList = findViewById(R.id.list_view_starters);
     }
 }
