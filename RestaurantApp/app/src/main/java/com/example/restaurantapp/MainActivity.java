3d425abbb44fec74931127fa65196411541f37aa
Veselin Velkov <vesselin60@gmail.com>
2022-12-13 11:46:53 +0200

    39. Intents - Moving to a new Activity

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.java
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.restaurantapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+        startersCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
