aec58975b5772ca29eaf45ca4c7b17a2d994571b
Veselin Velkov <vesselin60@gmail.com>
2022-12-14 15:49:12 +0200

    43. Creating the array for the Starter DIshes

--- RestaurantApp/app/src/main/java/com/example/restaurantapp/StartersActivity.java
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/StartersActivity.java
@@ -14,5 +14,28 @@ public class StartersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_starters);
 
         ListView startersList = findViewById(R.id.list_view_starters);
+
+        String[] starterDishes = {
+                "Nori and black pepper soup",
+                "Tofu and coconut soup",
+                "Tuna and truffle soup",
+                "Jalapeno and apple gyoza",
+                "Garam masala and ricotta parcels",
+                "Kiwi fruit and chilli dip",
+                "Vermicelli and celeriac soup",
+                "Trout and spinach wontons",
+                "Nectarine and cardamom dumplings",
+                "Mustard seed and pepper soup",
+                "Pigeon and spinach soup",
+                "Lamb and samphire soup",
+                "Tofu and ricotta gyoza",
+                "Parsley and trout dumplings",
+                "Cabbage and garam masala gyoza",
+                "Kohlrabi and orange soup",
+                "Saffron and sprout soup",
+                "Tofu and pepper uramaki",
+                "Shrimp and lemon soup",
+                "Parsley and sweetcorn gyoza"
+        };
     }
 }
