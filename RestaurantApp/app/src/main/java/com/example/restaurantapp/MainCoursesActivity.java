c791ef2dceb5c80cbe8187de1005c031bcccef53
Veselin Velkov <vesselin60@gmail.com>
2022-12-15 15:30:29 +0200

    48. Creating a new Activity class from scratch - Main Courses screen

---
+++ RestaurantApp/app/src/main/java/com/example/restaurantapp/MainCoursesActivity.java
@@ -0,0 +1,14 @@
+package com.example.restaurantapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainCoursesActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_courses);
+    }
+
+}
