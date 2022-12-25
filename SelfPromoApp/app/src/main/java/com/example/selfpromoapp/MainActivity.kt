11f83857d4ec05ce9b779a073459aa1c999af219
Veselin Velkov <vesselin60@gmail.com>
2022-12-25 08:29:54 +0200

    82. View references in Kotlin & Easier Button ClickListeners

--- SelfPromoApp/app/src/main/java/com/example/selfpromoapp/MainActivity.kt
+++ SelfPromoApp/app/src/main/java/com/example/selfpromoapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.selfpromoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+
+        }
+    }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val testString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_SHORT).show()
     }
 }
