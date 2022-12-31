4e9396c1883e8aa54c84b97e43c7949d48b052ab
Veselin Velkov <vesselin60@gmail.com>
2022-12-31 09:20:57 +0200

    98. Creating the Message class

--- SelfPromoApp/app/src/main/java/com/example/selfpromoapp/MainActivity.kt
+++ SelfPromoApp/app/src/main/java/com/example/selfpromoapp/MainActivity.kt
@@ -27,27 +27,27 @@ class MainActivity : AppCompatActivity() {
             onPreviewClicked()
         }
         val spinnerValues/*: Array<String>*/ = arrayOf("Android Developer", "Android Engineer")
-        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerValues)
+        val spinnerAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, spinnerValues)
         binding.spinnerJobTitle.adapter = spinnerAdapter
     }
 
     private fun onPreviewClicked() {
-        val contactName = binding.editTextContactName.text.toString()
-        val contactNumber = binding.editTextContactNumber.text.toString()
-        val myDisplayName = binding.editTextMyDisplayName.text.toString()
-        val includeJunior = binding.checkBoxJunior.isChecked
-        val jobTitle = binding.spinnerJobTitle.selectedItem?.toString()
-        val immediateStart = binding.checkBoxImmediateStart.isChecked
-        val startDate = binding.editTextStartDate.text.toString()
+
+        val message = Message(
+            binding.editTextContactName.text.toString(),
+            binding.editTextContactNumber.text.toString(),
+            binding.editTextMyDisplayName.text.toString(),
+            binding.checkBoxJunior.isChecked,
+            binding.spinnerJobTitle.selectedItem?.toString(),
+            binding.checkBoxImmediateStart.isChecked,
+            binding.editTextStartDate.text.toString()
+        )
+
 
         val previewActivityIntent = Intent(this, PreviewActivity::class.java)
-        previewActivityIntent.putExtra("Contact Name", contactName)
-        previewActivityIntent.putExtra("Contact Number", contactNumber)
-        previewActivityIntent.putExtra("My Display Name", myDisplayName)
-        previewActivityIntent.putExtra("Include Junior", includeJunior)
-        previewActivityIntent.putExtra("Job Title", jobTitle)
-        previewActivityIntent.putExtra("Immediate Start", immediateStart)
-        previewActivityIntent.putExtra("Start Date", startDate)
+        previewActivityIntent.putExtra("Message", message)
+        
         startActivity(previewActivityIntent)
     }
 }
---
+++ SelfPromoApp/app/src/main/java/com/example/selfpromoapp/Message.kt
@@ -0,0 +1,11 @@
+package com.example.selfpromoapp
+
+data class Message(
+    val contactName: String,
+    val contactNumber: String,
+    val myDisplayName: String,
+    val includeJunior: Boolean,
+    val jobTitle: String?,
+    val immediateStart: Boolean,
+    val startDate: String?
+)
