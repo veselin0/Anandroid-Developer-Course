3e64d157f11539bc908d086edc7a711bfd74563f
Veselin Velkov <vesselin60@gmail.com>
2023-01-03 16:50:26 +0200

    Play 102. 'lateinit' variables

--- SelfPromoApp/app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
+++ SelfPromoApp/app/src/main/java/com/example/selfpromoapp/PreviewActivity.kt
@@ -6,6 +6,9 @@ import com.example.selfpromoapp.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
 
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
+
     private lateinit var binding: ActivityPreviewBinding
     @Suppress("DEPRECATION")
 //    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -15,23 +18,35 @@ class PreviewActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val message = intent.getSerializableExtra("Message") as Message
+        displayMessage()
 
-//        val message = intent.getSerializableExtra("Message", Message::class.java)
+        setUpButton()
+    }
 
-        val messagePreviewText = """
-            
-            Hi, ${message.contactName},
-            My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
-            I have a portfolio of apps to demonstrate my technical skills
-            that I can show on request.
-            I am able to start a new position ${message.getAvailability()}.
-            Please, get in touch if you have any suitable roles for me.
-            Thanks and best regards.
-            
-        """.trimIndent()
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("Message") as Message
+
+        //        val message = intent.getSerializableExtra("Message", Message::class.java)
+
+        messagePreviewText = """
+                
+                Hi, ${message.contactName},
+                My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
+                I have a portfolio of apps to demonstrate my technical skills
+                that I can show on request.
+                I am able to start a new position ${message.getAvailability()}.
+                Please, get in touch if you have any suitable roles for me.
+                Thanks and best regards.
+                
+            """.trimIndent()
 
         binding.textViewMessage.text = messagePreviewText
+    }
+
+    private  fun setUpButton() {
+        binding.buttonSendMessage.setOnClickListener {
+            // Send message intent
+        }
     }
 }
 
