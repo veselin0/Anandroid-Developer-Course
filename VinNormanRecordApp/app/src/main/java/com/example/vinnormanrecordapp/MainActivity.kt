848d7077df63dcf8f1e1f67eb17761268912a179
Veselin Velkov <vesselin60@gmail.com>
2023-01-10 17:08:50 +0200

    128. Adding our first fragment

---
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.vinnormanrecordapp
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.vinnormanrecordapp.databinding.ActivityMainBinding
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportFragmentManager.commit {
+            add(R.id.frame_content, RunningFragment())
+        }
+    }
+}
