6586d3e262f72cee856a8cef9d567ddb469b6b70
Veselin <vesselin60@gmail.com>
2023-01-10 11:39:24 +0200

    125. ViewBinding - The current best way to find Views_2

--- RecordKeeperApp/app/src/main/java/com/example/recordkeeperapp/MainActivity.kt
+++ RecordKeeperApp/app/src/main/java/com/example/recordkeeperapp/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.recordkeeperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recordkeeperapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.frameContent
+        binding.bottomNav
     }
 
 }
