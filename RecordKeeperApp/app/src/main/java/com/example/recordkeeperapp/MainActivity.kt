6f92230db7537e5fac54afc2339f94a082f9b65a
Veselin <vesselin60@gmail.com>
2023-01-11 11:15:10 +0200

    133. Implementing the OnNavigationItemSelectedListener

--- RecordKeeperApp/app/src/main/java/com/example/recordkeeperapp/MainActivity.kt
+++ RecordKeeperApp/app/src/main/java/com/example/recordkeeperapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.recordkeeperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
 import com.example.recordkeeperapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,19 +18,32 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.buttonCycling.setOnClickListener { onCyclingCliclked() }
-        binding.buttonRunning.setOnClickListener { onCyclingRunning() }
+        @Suppress("DEPRECATION")
+        binding.bottomNav.setOnNavigationItemSelectedListener (this)
+
     }
 
-    private fun onCyclingRunning() {
+    private fun onRunningClicked() {
         supportFragmentManager.commit {
             replace(R.id.frame_content, RunningFragment())
         }
     }
 
-    private fun onCyclingCliclked() {
+    private fun onCyclingClicked() {
         supportFragmentManager.commit {
             replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
         }
     }
 
