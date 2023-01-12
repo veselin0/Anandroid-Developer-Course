cfa0174bfc14ad1ccdc2d07b408a345be77cb3e3
Veselin Velkov <vesselin60@gmail.com>
2023-01-12 10:28:21 +0200

    146. 'Deprecated' code solution of the problem

--- VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/MainActivity.kt
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.vinnormanrecordapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
 import com.example.vinnormanrecordapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-
-
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -19,8 +19,32 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    private fun onRunningClicked() {
         supportFragmentManager.commit {
-            add(R.id.frame_content, RunningFragment())
+            replace(R.id.frame_content, RunningFragment())
         }
     }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        } else {
+            return false
+        }
+    }
+
+
 }
