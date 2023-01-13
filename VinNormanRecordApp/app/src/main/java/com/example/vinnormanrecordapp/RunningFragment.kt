a74624e1495c14c502ec0c17af2c0072badad903
Veselin Velkov <vesselin60@gmail.com>
2023-01-13 15:06:27 +0200

    137. Viewbinding in Fragments

--- VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/RunningFragment.kt
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/RunningFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.vinnormanrecordapp.databinding.FragmentRunningBinding
 
 class RunningFragment : Fragment() {
+
+    private lateinit var binding: FragmentRunningBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_running, container, false)
+    ): View {
+
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
