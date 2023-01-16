732792bfadde3328baa085d0db8e5439e332110f
Veselin <vesselin60@gmail.com>
2023-01-16 10:19:05 +0200

    151. onCreate and onDestroy - and how to finish an Activity

--- ActivityLifeciclePlayground/app/src/main/java/com/example/activitylifecicleplaygroun/MainActivity.kt
+++ ActivityLifeciclePlayground/app/src/main/java/com/example/activitylifecicleplaygroun/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.activitylifecicleplaygroun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_exit).setOnClickListener {
+            Log.d("Gocho Kopchev", "In the button click listener...")
+            finish()
+        }
+        Log.d("GochoKochev", "I am in onCreate()!!")
     }
 
-    
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Gocho Kochev", "I am now in onDestroy()!!")
+    }
 }
