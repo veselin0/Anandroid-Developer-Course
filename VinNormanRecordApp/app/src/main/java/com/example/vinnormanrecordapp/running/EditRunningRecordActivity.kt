ae65b0ea869e5eb46c27f6a1585971ffa1fa8131
Veselin Velkov <vesselin60@gmail.com>
2023-02-14 17:16:21 +0200

    181. Types of SharedPreferences

--- VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
@@ -1,7 +1,10 @@
 package com.example.vinnormanrecordapp.running
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
 import com.example.vinnormanrecordapp.databinding.ActivityEditRunnungRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -14,9 +17,30 @@ class EditRunningRecordActivity : AppCompatActivity() {
         binding = ActivityEditRunnungRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val distance: String? = intent.getStringExtra("Distance")
+        val distance = intent.getStringExtra("Distance")
 
         title = "$distance Record"
+
+        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        applicationPreferences.edit {
+            putString("Some application data", "Application preference value here")
+        }
+
+        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+
+        activityPreferences.edit {
+            putInt("Some activity data", 15)
+        }
+
+        val fileNamePreferences = getSharedPreferences(
+            "some shared preference file", Context.MODE_PRIVATE
+        )
+
+        fileNamePreferences.edit {
+            putBoolean("some preference file name", false)
+        }
+
     }
 
 
