2a00d7fef29ff3c90441fa64c4e48bee93650a5f
Veselin Velkov <vesselin60@gmail.com>
2023-02-15 15:17:19 +0200

    182. Saving our records - Putting values into SharedPreferences

--- VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.edit
-import androidx.preference.PreferenceManager
 import com.example.vinnormanrecordapp.databinding.ActivityEditRunnungRecordBinding
 
 class EditRunningRecordActivity : AppCompatActivity() {
@@ -21,24 +20,44 @@ class EditRunningRecordActivity : AppCompatActivity() {
 
         title = "$distance Record"
 
-        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-        applicationPreferences.edit {
-            putString("Some application data", "Application preference value here")
+// EXAMPLES OF SHARED PREFERENCES
+//        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+//        applicationPreferences.edit {
+//            putString("Some application data", "Application preference value here")
+//        }
+//        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+//        activityPreferences.edit {
+//            putInt("Some activity data", 15)
+//        }
+//        val fileNamePreferences = getSharedPreferences(
+//            "some shared preference file", Context.MODE_PRIVATE
+//        )
+//        fileNamePreferences.edit {
+//            putBoolean("some preference file name", false)
+//        }
+
+        binding.buttonSave.setOnClickListener {
+            saveRecord(distance)
         }
 
-        val activityPreferences = getPreferences(Context.MODE_PRIVATE)
+    }
 
-        activityPreferences.edit {
-            putInt("Some activity data", 15)
-        }
+    private fun saveRecord(distance: String?) {
+        val record = binding.editTextRecord.text.toString()
+        val date = binding.editTextDate.text.toString()
 
-        val fileNamePreferences = getSharedPreferences(
-            "some shared preference file", Context.MODE_PRIVATE
-        )
+        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
 
-        fileNamePreferences.edit {
-            putBoolean("some preference file name", false)
+//        Old way to save data:
+//        val editor = runningPreferences.edit()
+//        editor.putString("record", record)
+//        editor.putString("date", date)
+//        editor.apply()
+
+        runningPreferences.edit {
+            putString("$distance record", record)
+            putString("$distance date", date)
         }
 
     }
