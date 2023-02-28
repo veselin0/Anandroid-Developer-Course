34a98b45ea615d77ac62b64e3c1c07ac91d7db55
Veselin Velkov <vesselin60@gmail.com>
2023-02-28 09:42:08 +0200

    190. Clearing a record - Removing values from SharedPreferences

--- VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
+++ VinNormanRecordApp/app/src/main/java/com/example/vinnormanrecordapp/running/EditRunningRecordActivity.kt
@@ -10,8 +10,13 @@ import com.example.vinnormanrecordapp.databinding.ActivityEditRunnungRecordBindi
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditRunnungRecordBinding
-    private lateinit var runningPreferences: SharedPreferences
-    private val distance: String? by lazy { intent.getStringExtra("Distance") }
+    private val runningPreferences by lazy {
+        getSharedPreferences(
+            "running",
+            Context.MODE_PRIVATE
+        )
+    }
+    private val distance by lazy { intent.getStringExtra("Distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +24,50 @@ class EditRunningRecordActivity : AppCompatActivity() {
         binding = ActivityEditRunnungRecordBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
+        setupUI()
 
+        displayRecord()
+    }
 
+    private fun setupUI() {
         title = "$distance Record"
+        binding.buttonSave.setOnClickListener {
+            saveRecord()
+            finish()
+        }
+        binding.buttonDelete.setOnClickListener {
+            clearRecord()
+            finish()
+        }
+    }
 
 
+    private fun displayRecord() {
+        binding.editTextRecord.setText(runningPreferences.getString("$distance record", null))
+        binding.editTextDate.setText(runningPreferences.getString("$distance date", null))
+    }
+
+    private fun saveRecord() {
+        val record = binding.editTextRecord.text.toString()
+        val date = binding.editTextDate.text.toString()
+
+        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
+
+        runningPreferences.edit {
+            putString("$distance record", record)
+            putString("$distance date", date)
+        }
+    }
+
+    private fun clearRecord() {
+        runningPreferences.edit {
+            remove("$distance record")
+            remove("$distance date")
+        }
+    }
+
+
+}
 // EXAMPLES OF SHARED PREFERENCES
 //        val applicationPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 //        applicationPreferences.edit {
@@ -41,38 +84,8 @@ class EditRunningRecordActivity : AppCompatActivity() {
 //            putBoolean("some preference file name", false)
 //        }
 
-        displayRecord()
-        binding.buttonSave.setOnClickListener {
-            saveRecord()
-            finish()
-        }
-    }
-
-
-    private fun displayRecord() {
-        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
-        binding.editTextRecord.setText(runningPreferences.getString("$distance record", null))
-        binding.editTextDate.setText(runningPreferences.getString("$distance date", null))
-    }
-
-    private fun saveRecord() {
-        val record = binding.editTextRecord.text.toString()
-        val date = binding.editTextDate.text.toString()
-
-        val runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
-
 //        Old way to save data:
 //        val editor = runningPreferences.edit()
 //        editor.putString("record", record)
 //        editor.putString("date", date)
 //        editor.apply()
-
-        runningPreferences.edit {
-            putString("$distance record", record)
-            putString("$distance date", date)
-        }
-
-    }
-
-
-}
